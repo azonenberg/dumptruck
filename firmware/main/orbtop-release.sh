f1bae4466b69e8e39b83128ecc5a4a0289f4abd9
Andrew Zonenberg <azonenberg@drawersteak.com>
2024-12-21 03:46:15 -0800

    Use OFLOW protocol for orbtop

--- firmware/main/orbtop-release.sh
+++ firmware/main/orbtop-release.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export OBJDUMP=arm-none-eabi-objdump
-orbtop --server localhost:6050 --elf-file release-build/bin/main --routines 20 --interval 1000 --cut-after=40
+orbtop --server localhost:6050 --elf-file release-build/bin/main --routines 20 --interval 1000 --cut-after=40 -p OFLOW
